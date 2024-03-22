ee782fdcca3892b4f376059e542d7233a2346d26
jsablonEnseignement <104369028+jsablonEnseignement@users.noreply.github.com>
2024-03-22 09:45:36 +0100

    Update students.service.ts

--- backend/src/services/students.service.ts
+++ backend/src/services/students.service.ts
@@ -1,32 +1,32 @@
 import { Types } from "mongoose";
-import { IStudent, Student } from "../models/students.model";
+import { Student, StudentModel } from "../models/students.model";
 
 const getStudents = async () => {
-  return await Student.find();
+  return await StudentModel.find();
 };
 
 const getStudent = async (id: string) => {
-  return await Student.findOne({ _id: new Types.ObjectId(id) });
+  return await StudentModel.findOne({ _id: new Types.ObjectId(id) });
 };
 
-const createStudent = async (studentToCreate: IStudent) => {
-  const newStudent = new Student(studentToCreate);
+const createStudent = async (studentToCreate: Student) => {
+  const newStudent = new StudentModel(studentToCreate);
   await newStudent.save();
   return getStudents();
 };
 
-const updateStudent = async (id: string, studentToUpdate: IStudent) => {
-  await Student.updateOne(
+const updateStudent = async (id: string, studentToUpdate: Student) => {
+  await StudentModel.updateOne(
     {
       _id: new Types.ObjectId(id),
     },
     studentToUpdate
   );
-  return getStudents();
+  return await getStudents();
 };
 
 const deleteStudent = async (id: string) => {
-  await Student.deleteOne({ _id: new Types.ObjectId(id) });
+  await StudentModel.deleteOne({ _id: new Types.ObjectId(id) });
 };
 
 export {
